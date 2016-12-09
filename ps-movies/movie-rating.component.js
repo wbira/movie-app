f6c2384ff753aa24f7468729f17dc4bc5ba92018
Waldemar Bira <waldek.bira@gmail.com>
2016-12-09 19:24:06 +0100

    Improved movieRaiting component, now it`s displaing max number of stars

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -3,22 +3,31 @@
 
     var module = angular.module("psMovies");
 
+    function buildEntries(value, max) {
+        var entires = [];
+        for (var i = 1; i <= max; i++) {
+            var icon = i <= value ? "glyphicon-star" : "glyphicon-star-empty";
+            entires.push(icon);
+        }
+        return entires;
+    }
+
     module.component("movieRating", {
         templateUrl: "ps-movies/movie-rating.component.html",
         bindings: {
-            value: "<"
+            value: "<",
+            max: "<"
         },
-        transclude: true,
         controllerAs: "model",
         controller: function () {
             var model = this;
 
             model.$onInit = function () {
-                model.entires = new Array(model.value);
+                model.entires = buildEntries(model.value, model.max);
             }
-            
+
             model.$onChanges = function () {
-                model.entires = new Array(model.value);
+                model.entires = buildEntries(model.value, model.max);
             }
         }
 

996bebc7131f9fb8a613e67c4bf31d684da983e3
Waldemar Bira <waldek.bira@gmail.com>
2016-12-09 16:22:13 +0100

    Created routing in old-fashion way

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,6 +1,17 @@
-(function() {
+(function () {
     "use strict";
 
-    angular.module("psMovies", []);
+    var module = angular.module("psMovies", ["ngRoute"]);
 
-}());
+    module.config(function routeConfig($routeProvider) {
+        $routeProvider
+            .when("/list", { template: "<movie-list></movie-list>" })
+            .when("/about", { template: "<app-about></app-about>" })
+            .otherwise({ redirectTo: "/list" })
+    });
+
+    module.component("appAbout", { 
+        template: "This is about page"
+    })
+
+} ());
